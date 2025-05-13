5ac7179e8c15d69f1095647372a3f64a88d4d38a
Dr. Qureshi <anique.qureshi@qc.cuny.edu>
2025-05-12 20:12:05 -0400

    added links

--- src/app/components/Menubar.jsx
+++ src/app/components/Menubar.jsx
@@ -1,10 +1,18 @@
+import Link from 'next/link'
+
 export default function Menubar() {
     return (
       <>
         <ul>
-          <li>Home</li>
-          <li>Midterm Part I</li>
-          <li>Midterm Part II</li>
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+          <li>
+            <a href='https://expense-green-alpha.vercel.app/' target='_blank'>Midterm Part I - Expense</a>
+          </li>
+          <li>
+            <a href='https://customer-weld.vercel.app/' target='_blank'>Midterm Part II Site</a>
+          </li>
         </ul>
       </>
     )
